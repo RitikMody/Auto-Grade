18db04129f7cff54703f972bc5c94176f5130e92
Ritik Mody <ritikashishmody@gmail.com>
2021-02-28 11:09:45 +0530

    deployed

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask , render_template, redirect
 import flask
 import pandas as pd
 import model
-import os
 import report
 
 app = Flask(__name__)
@@ -16,22 +15,16 @@ def upload_file():
 	uploaded_files = flask.request.files.getlist("file[]")
 	for uploaded_file in uploaded_files:
 		if uploaded_file.filename != '':
-			uploaded_file.save(f"./Users/{uploaded_file.filename}")
-			if uploaded_file.filename.endswith('.pdf'):
-				test_paper_name = uploaded_file.filename
-			if uploaded_file.filename.endswith('.csv'):
-				answer_key_name = uploaded_file.filename
+			uploaded_file.save(f"./User/{uploaded_file.filename}")
 
-	df = pd.read_csv(f"Users/{answer_key_name}")
-	answered = model.get_answers(f"Users/{test_paper_name}")
+	df = pd.read_csv("./User/answers-multipage.csv")
+	answered = model.get_answers("./User/test-multipage.pdf")
 	df["student_answer"] = pd.Series(answered)
 	print(df.head(), df.columns)
 	# df = pd.read_csv("./Data/corrected_sheet.csv")
 	num_total, num_correct, num_incorrect = report.calc_correct_responses(df)
 	topic_scores = report.calc_score_for_topic(df)
-	incorrect_question = report.incorrct_question(df)
-	os.remove(f"./Users/{answer_key_name}")
-	os.remove(f"./Users/{test_paper_name}")
+	incorrect_question = report.incorrct_question(df).reset_index()
 	return render_template('report.html', num_total = num_total, num_correct = num_correct, num_incorrect = num_incorrect, topic_scores = topic_scores,
 							question = incorrect_question['question'], answer = incorrect_question['correct_answer'], length = incorrect_question.shape[0])
 
